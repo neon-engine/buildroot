cca2b29bac989269d96121fb8a2bfd3be88661fa
Carlos Chulo <cchulo.dev@gmail.com>
2024-06-17 21:34:05 -0700

    downgraded glibc

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,7 +7,7 @@
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.38-44-gd37c2b20a4787463d192b32041c3406c2bd91de0
+GLIBC_VERSION = 2.28-211-g29f401e05d85919a5328567733867d9447d7d706
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
