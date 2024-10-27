71f744fbccb6b716bd42954e5e0577cb47b8bec5
Carlos Chulo <cchulo.dev@gmail.com>
2024-10-26 23:38:25 -0700

    the issue is resolved when upgrading to glibc 2.31, which this is the real version i should support since valve now supports the sniper runtime with the same glibc version

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,7 +7,7 @@
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.31-163-g8e30f03744837a85e33d84ccd34ed3abe30d37c3
+GLIBC_VERSION = 2.31-74-gd0c84d22b6a67f85a1eed3b93aef30e6953294b5
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
